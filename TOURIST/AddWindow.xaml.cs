92bbfe906c13a533b0f76ac29b8b9b822b5376f9
WHITE-LIME <dolgirev1607@gmail.com>
2021-05-25 17:04:47 +0300

    First Commint

---
+++ TOURIST/AddWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TOURIST
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public TOUR CurrentTOUR { get; set; }
+        public IEnumerable<AgentsT> agentsTs { get; set; }
+        public AddWindow(TOUR TOURsss)
+        {
+            InitializeComponent();
+            DataContext = this;
+            CurrentTOUR = TOURsss;
+            agentsTs = Core.DB.AgentsT.ToArray();
+        }
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (CurrentTOUR.AgentsT == null)
+                    throw new Exception("Не выбран агент");
+
+                if (CurrentTOUR.ID == 0)
+                    Core.DB.TOUR.Add(CurrentTOUR);
+
+                Core.DB.SaveChanges();
+
+                DialogResult = true;
+            }
+            catch
+            {
+                MessageBox.Show($"ERROR");
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ TOURIST/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TOURIST
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public IEnumerable<AgentsT> agentsTs { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IEnumerable<TOUR> _tOURs;
+        public IEnumerable<TOUR> tOURs
+        {
+            get
+            {
+                return _tOURs;
+            }
+            set
+            {
+                _tOURs = value;
+                if (PropertyChanged != null)
+                    {
+                    PropertyChanged(this, new PropertyChangedEventArgs("tOURs"));
+                    }
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            tOURs = Core.DB.TOUR.ToArray();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var addWindow = new AddWindow(new TOUR());
+
+            if (addWindow.ShowDialog() == true)
+            {
+                agentsTs = Core.DB.AgentsT.ToArray();
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var DeleteTours = TOURsName.SelectedItem as TOUR;
+
+            try
+            {
+                Core.DB.TOUR.Remove(DeleteTours);
+                Core.DB.SaveChanges();
+                tOURs = Core.DB.TOUR.ToArray();
+            }
+            catch
+            {
+                MessageBox.Show($"Ошибка");
+            }
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            var EditTourWindow = new AddWindow(TOURsName.SelectedItem as TOUR);
+            if (EditTourWindow.ShowDialog() == true)
+            {
+                tOURs = Core.DB.TOUR.ToArray();
+            }
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            var w = new MainWindow();
+            w.Show();
+
+            this.Close();
+        }
+    }
+}
