2be385a390ba678642f49aba0471113f8fe6d157
WHITE-LIME <dimakuraedov@gmail.com>
2021-05-28 17:30:14 +0300

    first commit

---
+++ TOURIST/AddWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TOURIST
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public static dEntities DB = new dEntities();
+        public Product CurrentProduct { get; set; }
+        public IEnumerable<ProductType> productTypesss { get; set; }
+
+
+        public AddWindow(Product productsss)
+        {
+            InitializeComponent();
+            DataContext = this;
+            CurrentProduct = productsss;
+            productTypesss = Core.DB.ProductType.ToArray();
+        }
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (CurrentProduct.ProductType == null)
+                    throw new Exception("Не выбран тип");
+
+                if (CurrentProduct.ID == 0)
+                    Core.DB.Product.Add(CurrentProduct);
+
+                Core.DB.SaveChanges();
+
+                DialogResult = true;
+
+                MessageBox.Show($"Сохранено");
+            }
+            catch
+            {
+                MessageBox.Show($"Ошибка");
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
